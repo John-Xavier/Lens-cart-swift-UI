9a4c820d44f58505beb308749b273d4142ed0698
John Xavier <johnxavier.sics@gmail.com>
2021-12-19 16:05:28 +0530

    Initial Commit

---
+++ Specs/SpecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecsApp.swift
+//  Specs
+//
+//  Created by John Srishti on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SpecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
